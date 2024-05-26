2ddf33efdc5f54d6b3697437cae75b0b2fd573cb
chagchagchag <chagchagchag.dev@gmail.com>
2024-05-27 06:56:07 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs ELK Study Log</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-elk-study-log',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-elk-study-log',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
